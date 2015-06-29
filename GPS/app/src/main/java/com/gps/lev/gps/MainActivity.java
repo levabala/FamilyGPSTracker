d32e0b5e4e4f57a97c651a6130b1c828059511dc
levabala <levabala@ya.ru>
2015-06-29 10:35:29 +0400

    MainActivity

--- GPS/app/src/main/java/com/gps/lev/gps/MainActivity.java
+++ GPS/app/src/main/java/com/gps/lev/gps/MainActivity.java
@@ -51,27 +51,27 @@ public class MainActivity extends ActionBarActivity {
 
     private LocationManager locationManager;
     StringBuilder sbGPS = new StringBuilder();
-    StringBuilder sbNet = new StringBuilder();
+    StringBuilder sbNet = new StringBuilder();                  //Здесь лежат какие-то переменные, я их не использую. Когда-ниубдь уберу :)
 
 
-    public void SwitchChanged(View v) {
+    public void SwitchChanged(View v) {   //Тестовая функция для отслеживания переключения свитча
         Switch rb = (Switch) v;
         Toast.makeText(this, "Отслеживание переключения: " + (rb.isChecked() ? "on" : "off"),
                 Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){   //Создание активити...
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        String entryWords = "<?xml version =\"1.0\" encoding=\"UTF-8\"?>\n" +
+        String entryWords = "<?xml version =\"1.0\" encoding=\"UTF-8\"?>\n" +  //Заголовок для xml-файла
                 "\n" +
                 "<gpx xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"1.0\"\n" +
                 " xmlns=\"http://www.topografix.com/GPX/1/0\" creator=\"LB company\" \n" +
                 "xsi:schemaLocation=\"http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd\"><time>" + formatDate(new Date()) + "</time><trk><name>GPS-трекер</name><trkseg>";
-        try{
+        try{  //Запись заголовка в файл хранения
             FileOutputStream fos = openFileOutput("Locations.data", Context.MODE_PRIVATE);
             fos.write(entryWords.getBytes());
             fos.close();
@@ -84,7 +84,7 @@ public class MainActivity extends ActionBarActivity {
         }
 
 
-        startService(
+        startService( //Запуск сервиса по отслеживанию геоданных
                 new Intent(MainActivity.this, MyService.class)
         );
     }
@@ -100,45 +100,41 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    protected void onDestroy() {
+    protected void onDestroy() { //При закрытии приложения остановить и сервис (не всегда срабатывает)
         super.onDestroy();
         stopService(
                 new Intent(MainActivity.this, MyService.class)
         );
-        Toast.makeText(this,"@string/app_name" + "закрыт", Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"@string/app_name " + "закрыт", Toast.LENGTH_LONG).show(); //Почему-то не работает получение имени приложения
     }
 
 
-    public void onClickLocationSittings(View view){
+    public void onClickLocationSittings(View view){  //Открытие настроек получения местоположения
         startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
     };
 
-    public void onClickOpenHistory(View view){
+    public void onClickOpenHistory(View view){ //Показать историю (тестовая функция)
         try {
             InputStream is = openFileInput("Locations.data");
             InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader reader = new BufferedReader(isr);
+            BufferedReader reader = new BufferedReader(isr);  //Чтение файла
             StringBuilder string = new StringBuilder();
             String line = "";
 
             while ((line = reader.readLine()) != null){
-                string.append(line);
+                string.append(line);  //Через StringBuilder добавляю строки в String
             }
 
             is.close();
 
-            Toast.makeText(this, string.toString() + "</trkseg></trk></gpx>", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, string.toString() + "</trkseg></trk></gpx>", Toast.LENGTH_SHORT).show(); //Показываю содержимое файла
         }
         catch (java.io.IOException e){
             Toast.makeText(this, "Ошибка работы с файлом: \n" + e, Toast.LENGTH_LONG).show();
         }
-
-        //Intent historyIntent = new Intent(MainActivity.this, MainActivity22Activity.class);
-        //historyIntent.putExtra("History",sLocations);
-        //startActivity(historyIntent);
     };
 
-    private void FinalWriting(){
+    private void FinalWriting(){  //Функция записи конца файла
         try{
             InputStream is = openFileInput("Locations.data");
             InputStreamReader isr = new InputStreamReader(is);
@@ -150,7 +146,7 @@ public class MainActivity extends ActionBarActivity {
                 string.append(line);
             }
 
-            String s = string.toString() + "</trkseg></trk></gpx>";
+            String s = string.toString() + "</trkseg></trk></gpx>";  //Вся суть в этих закрывающих тегах
             FileOutputStream fos = openFileOutput("Locations.data", Context.MODE_PRIVATE);
             fos.write(s.getBytes());
             fos.close();
@@ -164,7 +160,7 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    public String getHistoryFile(){
+    public String getHistoryFile(){ //Быстрое получение содержимого файла хранения (для последующей отправки)
         try{
             InputStream is = openFileInput("Locations.data");
             InputStreamReader isr = new InputStreamReader(is);
@@ -190,7 +186,7 @@ public class MainActivity extends ActionBarActivity {
         return "";
     }
 
-    Boolean SendingSuccefull = true;
+    Boolean SendingSuccessful = true;  //Эта штука пока что не работает
     public void SendPathToServer(View v){
         try {
             //Toast.makeText(this,"Задача создана", Toast.LENGTH_LONG).show();
@@ -199,11 +195,11 @@ public class MainActivity extends ActionBarActivity {
         }
         catch (Exception e) {Toast.makeText(this,"Ошибка: " + e, Toast.LENGTH_LONG).show();}
 
-        if (SendingSuccefull) {
-            Toast.makeText(this,"Новый маршрут",Toast.LENGTH_LONG).show();
+        if (SendingSuccessful) { //Это надо заменить на EventListener. Генерить событие в AsyncTask
+            Toast.makeText(this,"Новый маршрут",Toast.LENGTH_LONG).show(); //Очистка маршрута. Надо диалоговое окошко выбора сделать
             try {
                 FileOutputStream fos = openFileOutput("Locations.data", Context.MODE_PRIVATE);
-                String c = "<?xml version =\"1.0\" encoding=\"UTF-8\"?>\n" +
+                String c = "<?xml version =\"1.0\" encoding=\"UTF-8\"?>\n" +      //Опять заголовок... Пора его куда-нибудь вынести
                         "\n" +
                         "<gpx xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" version=\"1.0\"\n" +
                         " xmlns=\"http://www.topografix.com/GPX/1/0\" creator=\"LB company\" \n" +
@@ -217,7 +213,7 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    private String formatDate(Date d){
+    private String formatDate(Date d){ //Форматирование даты под формат GPX
         SimpleDateFormat[] formats = new SimpleDateFormat[]{
                 new SimpleDateFormat("yyyy-MM-dd"),
                 new SimpleDateFormat("kk:mm:ss")
@@ -231,7 +227,7 @@ public class MainActivity extends ActionBarActivity {
     };
 
 
-    private class HttpReq extends AsyncTask<String,Void,String>{
+    private class HttpReq extends AsyncTask<String,Void,String>{ //Асинхронная задача отправки маршрута
         @Override
         protected void onProgressUpdate(Void... values) {
             super.onProgressUpdate(values);
@@ -241,63 +237,37 @@ public class MainActivity extends ActionBarActivity {
         protected void onPostExecute(String aString) {
             super.onPostExecute(aString);
             Toast.makeText(getApplicationContext(),"Статус завершения подключения: " + aString, Toast.LENGTH_LONG).show();
-            if (aString == "Ошибка") SendingSuccefull = false;
+            if (aString == "Ошибка") SendingSuccessful = false; //Надо заменить на создание callback
         }
 
         @Override
         protected String doInBackground(String... params) {
             String inf = "";
-            try{
+            try{  //Ну а тут начинатся белеберда с http-запросом :)
                 Switch rb = (Switch)findViewById(R.id.switch1);
                 String answer = "";
 
                 URL url;
-                if (rb.isChecked()) url = new URL("http://192.168.3.5:3000/android");//?path=" + params[0]);
-                else url = new URL("http://62.84.166.86:3000/android");//?path=" + params[0]);
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                if (rb.isChecked()) url = new URL("http://192.168.3.5:3000/android");  //Генерю URL
+                else url = new URL("http://62.84.166.86:3000/android");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection(); //Открываю соединение
+
                 connection.setRequestMethod("POST");
                 connection.setDoOutput(true);
-                connection.setConnectTimeout(5000);
+                connection.setConnectTimeout(5000);  //Задаю параметры запроса
                 connection.setReadTimeout(5000);
+
                 DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
-                outputStream.write(params[0].getBytes());
-                answer += connection.getRequestMethod() + "\n";
-                answer += connection.getResponseCode() + "\n";
-                answer += connection.getResponseMessage() + "\n";
+                outputStream.write(params[0].getBytes()); //Пишу трек в выходящий поток
+
                 connection.disconnect();
-                outputStream.close();
-                return String.valueOf(connection.getResponseMessage());
+                outputStream.close(); //Закрываю соединение
+
+                return String.valueOf(connection.getResponseMessage());  //Если всё ок, вывожу ответ сервера (должен быть Accepted)
             }
             catch (Exception e){
                 return "Ошибка";
             }
-
-            /*Switch rb = (Switch)findViewById(R.id.switch1);
-            String url;
-            if (rb.isChecked()) url = "http://192.168.3.5:3000/android?path=" + params[0];
-            else url = "http://62.84.166.86:3000/android?path=" + params[0];
-            WebView webView = (WebView)findViewById(R.id.webView);
-            webView.setWebViewClient(new MyWebViewClient());
-            webView.loadUrl(url);
-            return "All rigth";*/
         }
-    }
-
-    private class MyWebViewClient extends WebViewClient
-    {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url)
-        {
-            view.loadUrl(url);
-            return true;
-        }
-    }
-
-    public void ChangeState(View v){
-        RadioButton rb = (RadioButton) v;
-        Toast.makeText(this,String.valueOf(rb.isChecked()),Toast.LENGTH_LONG).show();
-
-        if (rb.isChecked()) rb.setChecked(false); else rb.setChecked(true);
-        Toast.makeText(this,String.valueOf(rb.isChecked()),Toast.LENGTH_LONG).show();
     }
 }
